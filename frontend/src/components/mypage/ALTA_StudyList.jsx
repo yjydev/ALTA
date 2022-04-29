687abf9638e72181a8cbfd3a8286c57212aa7f1a
윤지영 <yun22287@naver.com>
2022-04-29 10:45:01 +0900

    S06P31B203-215 feat: 🎸 초대코드 입력 아코디언 삭제 & 직접 구현

--- frontend/src/components/mypage/ALTA_StudyList.jsx
+++ frontend/src/components/mypage/ALTA_StudyList.jsx
@@ -2,14 +2,14 @@ import { Box, Grid } from '@mui/material';
 
 import ALTA_ContentsTitle from '../common/ALTA_ContentsTitle';
 import ALTA_StudyCard from './ALTA_StudyCard';
-import ALTA_inviteAccordions from './ALTA_inviteAccordion';
+import ALTA_inviteInput from './ALTA_inviteInput';
 
 export default function ALTA_StudyList() {
   return (
     <Box sx={wrapper}>
       <ALTA_ContentsTitle>스터디 그룹 목록</ALTA_ContentsTitle>
       <Grid mb={3} align="right">
-        <ALTA_inviteAccordions />
+        <ALTA_inviteInput />
       </Grid>
       <Grid sx={studyListStyle} container spacing={3} mb={3}>
         {[0, 0, 0].map((v, i) => (
---
+++ frontend/src/components/mypage/ALTA_inviteInput.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+import { Button, Typography, TextField, Box, Grid } from '@mui/material';
+
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
+export default function ALTA_inviteInput() {
+  const [isToggle, handleisToggle] = useState(false);
+
+  return (
+    <Box sx={wrapper}>
+      <Box>
+        <Button
+          onClick={() => handleisToggle(!isToggle)}
+          sx={inputBtn}
+          endIcon={
+            isToggle ? <ArrowBackIosNewIcon /> : <ArrowForwardIosSharpIcon />
+          }
+        >
+          {/* {isToggle ? null : (
+            <Typography sx={inputText}>초대코드 입력</Typography>
+          )} */}
+          <Typography sx={inputText}>초대코드 입력</Typography>
+        </Button>
+      </Box>
+      <Box sx={[fieldStyle, isToggle ? { width: '250px' } : { width: '0px' }]}>
+        <Box>
+          <TextField
+            required
+            sx={{
+              width: '100%',
+            }}
+            variant="standard"
+            placeholder="초대코드를 입력해주세요"
+          />
+        </Box>
+        <Box sx={completeBtn}>
+          {isToggle ? <Button variant="outlined">완료</Button> : null}
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+const wrapper = {
+  display: 'flex',
+  margin: '10px 0',
+  minHeight: '50px',
+  justifyContent: 'right',
+};
+
+const inputText = {
+  marginRight: 1,
+  fontWeight: 'bold',
+};
+
+const inputBtn = {
+  color: '#212121',
+};
+
+const completeBtn = {
+  marginLeft: 2,
+};
+
+const fieldStyle = {
+  display: 'flex',
+  transition: '.4s',
+  marginLeft: 2,
+};
