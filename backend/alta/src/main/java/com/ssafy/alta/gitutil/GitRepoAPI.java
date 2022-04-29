c1e544b6fa7244689f1061a93e8153e78e31b950
김유진 <determination9212@gmail.com>
2022-04-29 11:38:31 +0900

    S06P31B203-227 feat: 🎸 서버 내부에서 함수를 호출해서 repo를 생성할 수 있는 기능 추가

---
+++ backend/alta/src/main/java/com/ssafy/alta/gitutil/GitRepoAPI.java
@@ -0,0 +1,47 @@
+package com.ssafy.alta.gitutil;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.alta.config.GithubConfig;
+import com.ssafy.alta.dto.request.GithubRepoRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+/**
+ * packageName 	: com.ssafy.alta.gitutil
+ * fileName 	: GitRepoAPI
+ * author 	    : 김유진
+ * date		    : 2022-04-29
+ * description	:
+ * ===========================================================
+ * DATE 		    AUTHOR 		        NOTE
+ * -----------------------------------------------------------
+ * 2022-04-29	        김유진  		        최초 생성
+ */
+public class GitRepoAPI {
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public String insertRepo(String token, GithubRepoRequest githubRepoRequest) throws JsonProcessingException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(GithubConfig.AUTH, "token " + token);
+        httpHeaders.set("Accept", "application/vnd.github.v3+json");
+        httpHeaders.set("Content-Type", "application/json");
+
+        String jsonBody = new ObjectMapper().writeValueAsString(githubRepoRequest);
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(jsonBody, httpHeaders);
+        String url = "https://api.github.com/user/repos";
+
+        ResponseEntity<HashMap> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, HashMap.class);
+
+        for (Object key : response.getBody().keySet()) {
+            System.out.println( key + " : " + (response.getBody().get(key)));
+        }
+        return  response.getStatusCode().toString();
+    }
+}
