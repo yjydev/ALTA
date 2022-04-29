8facfb26d66e23713095bfcbe7f5f7752b536acb
WooJeongYeon <wjygogogo@gmail.com>
2022-04-29 14:40:10 +0900

    S06P31B203-237 feat: 🎸 백엔드 - 댓글 작성 기능 추가

--- backend/alta/src/main/java/com/ssafy/alta/service/CommentService.java
+++ backend/alta/src/main/java/com/ssafy/alta/service/CommentService.java
@@ -1,15 +1,21 @@
 package com.ssafy.alta.service;
 
+import com.ssafy.alta.dto.request.CommentRequest;
 import com.ssafy.alta.dto.response.CommentResponse;
 import com.ssafy.alta.entity.Code;
 import com.ssafy.alta.entity.Comment;
+import com.ssafy.alta.entity.User;
+import com.ssafy.alta.exception.DataNotFoundException;
+import com.ssafy.alta.repository.CodeRepository;
 import com.ssafy.alta.repository.CommentRepository;
+import com.ssafy.alta.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * packageName 	: com.ssafy.alta.service
@@ -27,6 +33,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final CodeRepository codeRepository;
 
     public List<CommentResponse> selectCommentList(Code code) {
         List<Comment> commentList = commentRepository.findCommentsByCodeOrderByCreateDateDesc(code);
@@ -35,5 +43,18 @@ public class CommentService {
             commentResponseList.add(comment.toDto());
         }
         return commentResponseList;
+    }
+
+    @Transactional
+    public void insertComment(String userId, CommentRequest commentRequest) {
+        Optional<Code> optCode = Optional.ofNullable(codeRepository.findById(commentRequest.getCodeId())
+                .orElseThrow(DataNotFoundException::new));
+        Optional<User> optUser = Optional.ofNullable(userRepository.findById(userId)).orElseThrow(DataNotFoundException::new);
+        Code code = optCode.get();
+        User user = optUser.get();
+
+        Comment comment = commentRequest.toEntity(user, code);
+        commentRepository.save(comment);
+
     }
 }
