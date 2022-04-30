6e2a68f23ba0d8bf58ba5713dfffcd43e4aaa423
오서하 <seoha8952@gmail.com>
2022-05-01 02:20:36 +0900

    S06P31B203-249 feat: 🎸 유저id 반환 함수 구현

--- backend/alta/src/main/java/com/ssafy/alta/controller/UserController.java
+++ backend/alta/src/main/java/com/ssafy/alta/controller/UserController.java
@@ -1,23 +1,11 @@
 package com.ssafy.alta.controller;
 
-import com.ssafy.alta.config.oauth.PrincipalDetails;
-import com.ssafy.alta.entity.User;
 import com.ssafy.alta.repository.UserRepository;
 import com.ssafy.alta.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/user")
@@ -37,8 +25,8 @@ public class UserController {
 
 
     @GetMapping("/test")
-    public void getUserInfo() {
-        System.out.println("enter?!!??!?");
+    public ResponseEntity getUserInfo( @RequestHeader String Authorization) {
+        return new ResponseEntity<>(userService.getCurrentUsername(), HttpStatus.OK);
     }
 
 }
--- backend/alta/src/main/java/com/ssafy/alta/service/UserService.java
+++ backend/alta/src/main/java/com/ssafy/alta/service/UserService.java
@@ -5,6 +5,9 @@ import com.ssafy.alta.jwt.util.SecurityUtil;
 import com.ssafy.alta.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +21,23 @@ public class UserService {
     @Transactional(readOnly = true)
     public Optional<User> getUserWithAuthorities(String username) {
         return Optional.ofNullable(userRepository.findByName(username));
+    }
+
+    public Optional<String> getCurrentUsername() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            return Optional.empty();
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
     }
 }
