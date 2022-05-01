bee534d9d19a9cd0ce7986ae35d97c66f1c2c229
WooJeongYeon <wjygogogo@gmail.com>
2022-05-02 08:36:37 +0900

    S06P31B203-230 docs: ✏️ 백엔드 - 일부 주석 빼먹음

--- backend/alta/src/main/java/com/ssafy/alta/controller/CodeController.java
+++ backend/alta/src/main/java/com/ssafy/alta/controller/CodeController.java
@@ -21,7 +21,7 @@ public class CodeController {
     private final CodeService codeService;
 
     @PostMapping
-    //@ApiOperation(value = "코드 추가", notes = "새 코드를 DB에 생성하고 Github에 commit합니다.")
+    @ApiOperation(value = "코드 추가", notes = "새 코드를 DB에 생성하고 Github에 commit합니다.")
     public ResponseEntity insertCode(@ApiParam(value = "스터디 키", required = true) @PathVariable("study_id") Long studyId, @ApiParam(value = "코드 추가 요청 정보", required = true)  @RequestBody CodeRequest codeRequest) throws JsonProcessingException {
         String userId = "11";
         String token = "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";
@@ -30,7 +30,7 @@ public class CodeController {
     }
 
     @GetMapping("/{code_id}")
-    //@ApiOperation(value = "코드와 댓글 조회", notes = "Github에서 일어난 코드 변경을 반영하고 코드와 관련 댓글들을 조회합니다.")
+    @ApiOperation(value = "코드와 댓글 조회", notes = "Github에서 일어난 코드 변경을 반영하고 코드와 관련 댓글들을 조회합니다.")
     public ResponseEntity selectCodeAndComments(@ApiParam(value = "스터디 키", required = true) @PathVariable("study_id") Long studyId, @ApiParam(value = "코드 키", required = true) @PathVariable("code_id") Long codeId) throws JsonProcessingException {
         String token = "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";
         CodeAndCommentResponse codeAndCommentResponse = codeService.selectCodeAndComments(studyId, codeId, token);
@@ -38,7 +38,7 @@ public class CodeController {
     }
 
     @DeleteMapping("/{code_id}")
-    //@ApiOperation(value = "코드 삭제", notes = "Github에서 일어난 sha값 변경을 반영하고 코드를 DB에서 삭제합니다.")
+    @ApiOperation(value = "코드 삭제", notes = "Github에서 일어난 sha값 변경을 반영하고 코드를 DB에서 삭제합니다.")
     public ResponseEntity deleteCode(@ApiParam(value = "스터디 키", required = true) @PathVariable("study_id") Long studyId, @ApiParam(value = "코드 키", required = true) @PathVariable("code_id") Long codeId) throws JsonProcessingException {
         String userId = "11";
         String token = "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";
