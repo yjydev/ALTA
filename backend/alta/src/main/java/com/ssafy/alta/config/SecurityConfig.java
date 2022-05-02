a86aa670a2c85b560f463390a95a8526b130da75
WooJeongYeon <wjygogogo@gmail.com>
2022-05-03 06:33:02 +0900

    S06P31B203-324 fix: 🐛 백엔드 - 코드 삭제 수정

--- backend/alta/src/main/java/com/ssafy/alta/config/SecurityConfig.java
+++ backend/alta/src/main/java/com/ssafy/alta/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // 모든 요청은 인증이 되어야 하지만, 로그인관련하여 요청은 다 권한은 허락해주어야 한다.
                     .authorizeRequests()
-                    .antMatchers("/api/user/gitLogin/**").permitAll()
+                    .antMatchers("/api/**").permitAll()
                     .anyRequest().authenticated()
 //                .anyRequest().permitAll()
 
