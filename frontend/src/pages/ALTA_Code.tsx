64512d1394b36778c7e81568983bdd702ec33df5
윤지영 <yun22287@naver.com>
2022-05-03 11:27:08 +0900

    fix: 🐛 코드 리뷰 반영 & (댓글 작성 기능 구현중)

--- frontend/src/pages/ALTA_Code.tsx
+++ frontend/src/pages/ALTA_Code.tsx
@@ -5,15 +5,15 @@ import ALTA_CodeContents from '../components/code/ALTA_CodeContents';
 import ALTA_Template from '../components/common/ALTA_Template';
 import ALTA_Header from '../components/common/ALTA_Header';
 
-import CodeReviewProvider from '../context/CodeReviewProvider';
+import CodeReviewContext from '../context/CodeReviewContext';
 
 export default function ALTA_Code() {
   const param = useParams();
   const Header = () => <ALTA_Header />;
   const Contents = () => (
-    <CodeReviewProvider>
+    <CodeReviewContext>
       <ALTA_CodeContents param={param} />
-    </CodeReviewProvider>
+    </CodeReviewContext>
   );
   return <ALTA_Template header={<Header />} contents={<Contents />} />;
 }
