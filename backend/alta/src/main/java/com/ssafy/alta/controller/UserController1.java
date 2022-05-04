a7a8f1ff7686d3761418116b5d521897f79cb365
김유진 <determination9212@gmail.com>
2022-05-05 00:40:20 +0900

    S06P31B203-300 feat: 🎸 유저 정보 수정하는 controller 추가

--- backend/alta/src/main/java/com/ssafy/alta/controller/UserController1.java
+++ backend/alta/src/main/java/com/ssafy/alta/controller/UserController1.java
@@ -7,10 +7,7 @@ import com.ssafy.alta.service.UserService1;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * packageName 	: com.ssafy.alta.controller
@@ -35,5 +32,11 @@ public class UserController1 {
     @ApiOperation(value = "user 정보 가져오기", notes = "user 정보 가져고익")
     public ResponseEntity selectUserInfo() {
         return new ResponseEntity<>(userService1.selectUser(), HttpStatus.OK);
+    }
+
+    @PatchMapping("/info")
+    @ApiOperation(value = "user 개인정보 수정", notes ="user 개인 정보 수정")
+    public ResponseEntity updateUserInfo(@RequestBody UserUpdateRequest userUpdateRequest) {
+        return new ResponseEntity(userUpdateRequest, )
     }
 }
