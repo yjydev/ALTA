86e46b299f66b5ba78bf75ec62ce26e93cdf3a85
오서하 <seoha8952@gmail.com>
2022-05-10 16:21:37 +0900

    S06P31B203-378 feat: 🎸 성실 점수를 위한 entity, service 생성

---
+++ backend/alta/src/main/java/com/ssafy/alta/repository/ActivityScoreRepository.java
@@ -0,0 +1,18 @@
+package com.ssafy.alta.repository;
+
+import com.ssafy.alta.entity.ActivityScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName 	: com.ssafy.alta.repository
+ * fileName 	: ActivityScoreRepository
+ * author 	    : 오서하
+ * date		    : 2022-05-10
+ * description	:
+ * ===========================================================
+ * DATE 		AUTHOR 		      NOTE
+ * -----------------------------------------------------------
+ * 2022-05-10	    오서하  		    최초 생성
+ */
+public interface ActivityScoreRepository extends JpaRepository<ActivityScore, Long> {
+}
---
+++ backend/alta/src/main/java/com/ssafy/alta/service/ActivityScoreService.java
@@ -0,0 +1,20 @@
+package com.ssafy.alta.service;
+
+/**
+ * packageName 	: com.ssafy.alta.service
+ * fileName 	: ActivityScoreService
+ * author 	    : 오서하
+ * date		    : 2022-05-10
+ * description	: 성실점수
+ * ===========================================================
+ * DATE 		AUTHOR 		      NOTE
+ * -----------------------------------------------------------
+ * 2022-05-10	    오서하  		    최초 생성
+ */
+public class ActivityScoreService {
+
+    public void addScoreForComment(String user_id, Long study_id, Long code_id){
+
+    }
+
+}
