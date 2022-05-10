95ddd919abfa7198387e13a3b2ac1d848308124c
Ju Hyeon <aganga7427@gmail.com>
2022-05-10 15:42:19 +0900

    S06P31B203-388 refactor: 💡 axios 요청함수 모듈화

--- frontend/src/components/code/ALTA_CodeSubmitContents.tsx
+++ frontend/src/components/code/ALTA_CodeSubmitContents.tsx
@@ -10,6 +10,7 @@ import { blackColor } from '../../modules/colorChart';
 import { generateError } from '../../modules/generateAlert';
 import { postRequest, putRequest } from '../../api/request';
 import { checkLogin } from '../../modules/LoginTokenChecker';
+import { editCodeApi, submitCodeApi } from '../../api/apis';
 
 export default function ALTA_CodeSubmitContents() {
   const navigate = useNavigate();
@@ -18,7 +19,6 @@ export default function ALTA_CodeSubmitContents() {
   const problemId = JSON.parse(state).problemId;
   const fileName = JSON.parse(state).fileName;
   const studyId = JSON.parse(state).studyId;
-  //API 명세를 보니 codeId가 필요하더군요. 57번 라인을 확인해주세요
   const codeId = JSON.parse(state).codeId;
 
   const [commitMessage, setCommitMessage] = useState<string>('');
@@ -48,31 +48,14 @@ export default function ALTA_CodeSubmitContents() {
       return;
     }
 
-    await checkLogin();
+    if (!(await checkLogin())) navigate('/');
 
-    const requestBody = {
-      commitMessage,
-      fileName,
-      content: code,
-    };
-    // 코드를 최초 제출할 때는 problem Id를 body에 담아서 보내줘야 합니다.
-    // 따라서 problemId를 router의 parameter로 보내줍니다.
-    // 코드 재업로드를 위해 코드 업로드 페이지로 접근할 경우에는 parameter에서 problem Id를 제외하고 code id를 보내주세요
-    // problemId의 유무로 분기처리됩니다.
     if (problemId) {
-      await postRequest(
-        `/api/study/${studyId}/code`,
-        Object.assign(requestBody, { problemId }),
-      );
+      await submitCodeApi(studyId, problemId, commitMessage, fileName, code);
 
       goStudyDetail();
     } else {
-      await putRequest(
-        `/api/study/${studyId}/code/${codeId}/reupload`,
-        Object.assign(requestBody),
-      );
-
-      // 코드 상세 페이지로 이동하면 될 것 같습니다
+      await editCodeApi(studyId, codeId, commitMessage, fileName, code);
     }
   };
 
--- frontend/src/components/study/builder/ALTA_AddProblemBarBuilder.tsx
+++ frontend/src/components/study/builder/ALTA_AddProblemBarBuilder.tsx
@@ -5,11 +5,10 @@ import styled from '@emotion/styled';
 
 import { blackColor, subColor, whiteColor } from '../../../modules/colorChart';
 import { generateError } from '../../../modules/generateAlert';
-import { postRequest, putRequest } from '../../../api/request';
 import { StudyDetailStore } from '../../../context/StudyDetailContext';
 import { checkLogin } from '../../../modules/LoginTokenChecker';
 import { useNavigate } from 'react-router-dom';
-import { Problem } from '../../../types/StudyType';
+import { addProblemApi, editProblemApi } from '../../../api/apis';
 
 export const addProblemBarFrontBuilder = () =>
   function Front({ fliper }: { fliper: () => void }) {
@@ -58,11 +57,7 @@ export const addProblemBarBackBuilder = (
     const [problemLink, setPropblemLink] = useState<string>(link ? link : '');
 
     const addProblem = async () => {
-      (async function () {
-        await checkLogin();
-      })();
-
-      console.log(studyId, problemId);
+      if (!(await checkLogin())) navigate('/');
 
       //unix 시간을 비교하여 시작 > 마감의 경우 예외 처리
       if (!problemName || !problemLink) {
@@ -70,34 +65,22 @@ export const addProblemBarBackBuilder = (
         return;
       }
 
-      const requestBody = {
-        problems: [
-          {
-            name: problemName,
-            link: problemLink,
-          },
-        ],
-        scheduleId,
-      };
+      await addProblemApi(studyId, scheduleId, problemName, problemLink);
 
-      await postRequest(`/api/study/${studyId}/problem`, requestBody);
       setPropblemName('');
       setPropblemLink('');
       getStudyDetail(studyId);
     };
 
     const editProblem = async () => {
+      if (!(await checkLogin())) navigate('/');
+
       if (name && name === problemName && link && link === problemLink) {
         generateError('변경 사항이 없습니다', '');
         return;
       }
-      const requestBody = {
-        id: problemId,
-        name: problemName,
-        link: problemLink,
-      };
 
-      await putRequest(`/api/study/${studyId}/problem/`, requestBody);
+      await editProblemApi(studyId, problemId, problemName, problemLink);
       fliper();
       getStudyDetail(studyId);
     };
