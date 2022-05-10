f213226cccad1bc3747b6f97c1856c34943e279a
이지순 <jisoon806@gmail.com>
2022-05-10 17:18:26 +0900

    S06P31B203-416 feat: 🎸 swagger annotation 추가

--- backend/alta/src/main/java/com/ssafy/alta/controller/NoticeController.java
+++ backend/alta/src/main/java/com/ssafy/alta/controller/NoticeController.java
@@ -2,6 +2,7 @@ package com.ssafy.alta.controller;
 
 import com.ssafy.alta.dto.request.NoticeRequest;
 import com.ssafy.alta.service.NoticeService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,12 +28,14 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @PostMapping
+    @ApiOperation(value = "공지사항 추가", notes = "공지사항을 추가합니다. 공지사항 내용만 입력받습니다.")
     public ResponseEntity insertNotice(@PathVariable("studyId") Long studyId, @RequestBody NoticeRequest noticeRequest) {
         noticeService.insertNotice(studyId, noticeRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
+    @ApiOperation(value = "공지사항 조회", notes = "공지사항을 조회합니다. 최신업데이트된 내용을 가져옵니다.")
     public ResponseEntity selectNotice(@PathVariable("studyId") Long studyId) throws ParseException {
         return new ResponseEntity<>(noticeService.selectNotice(studyId), HttpStatus.OK);
     }
