3494ee4069e50ad4fe668ddd78a68f34b19ab025
윤지영 <yun22287@naver.com>
2022-05-12 04:41:36 +0900

    feat: 🎸 멤버 초대 취소 기능 구현

--- frontend/src/api/apis.ts
+++ frontend/src/api/apis.ts
@@ -231,3 +231,8 @@ export async function deleteCodeApi(studyId: number, codeId: number) {
 export async function deleteReviewApi(reviewId: number) {
   return await deleteRequest(`/api/code/review/${reviewId}`);
 }
+
+// 초대 대기 삭제 요청
+export async function deleteInvitationApi(studyId: number, sjiId: number) {
+  return await deleteRequest(`/api/study/${studyId}/invitation/${sjiId}`);
+}
