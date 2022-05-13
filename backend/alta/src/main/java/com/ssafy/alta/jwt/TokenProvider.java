59a7ede4f2c0b9e03a9551e5774e9f5391614789
오서하 <seoha8952@gmail.com>
2022-05-13 15:32:02 +0900

    S06P31B203-383 refactor: 💡 리펙토링

--- backend/alta/src/main/java/com/ssafy/alta/jwt/TokenProvider.java
+++ backend/alta/src/main/java/com/ssafy/alta/jwt/TokenProvider.java
@@ -6,6 +6,7 @@ import com.ssafy.alta.service.UserService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
