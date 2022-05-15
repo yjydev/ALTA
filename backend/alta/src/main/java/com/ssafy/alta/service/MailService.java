f4e36100ca919bd07cf627359d63d2bd7fbdd9ca
WooJeongYeon <wjygogogo@gmail.com>
2022-05-16 08:18:12 +0900

    S06P31B203-430 feat: 🎸 백엔드 - 메일 서비스 관련 추가

---
+++ backend/alta/src/main/java/com/ssafy/alta/service/MailService.java
@@ -0,0 +1,44 @@
+package com.ssafy.alta.service;
+
+import com.ssafy.alta.mailutil.MailHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+
+/**
+ * packageName 	: com.ssafy.alta.service
+ * fileName 	: MailService
+ * author 	    : 우정연
+ * date		    : 2022-05-16
+ * description	: 메일 서비스
+ * ===========================================================
+ * DATE 		AUTHOR 		      NOTE
+ * -----------------------------------------------------------
+ * 2022-05-16	    우정연  		    최초 생성
+ */
+
+@Component
+@RequiredArgsConstructor
+public class MailService {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    public void sendAlertMail(String email, String message, String url) throws MessagingException {
+        MailHandler mailHandler = new MailHandler(mailSender);
+        mailHandler.setTo(email);
+        mailHandler.setFrom("alta.invitation@gmail.com");
+        mailHandler.setSubject("ALTA에서 알림이 도착했습니다.");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("url", url);
+        String html = templateEngine.process("alertTemplate", context);
+        mailHandler.setText(html, true);
+
+        mailHandler.send();
+    }
+}
