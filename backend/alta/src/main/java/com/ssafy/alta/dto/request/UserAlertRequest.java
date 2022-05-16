16cb52c65c81246ff9801d73aee49bcbe2f91770
김유진 <determination9212@gmail.com>
2022-05-17 08:41:03 +0900

    S06P31B203-372 feat: 🎸 유저 알람 정보를 받아들일 객체 생성

--- backend/alta/src/main/java/com/ssafy/alta/dto/request/UserAlertRequest.java
+++ backend/alta/src/main/java/com/ssafy/alta/dto/request/UserAlertRequest.java
@@ -1,13 +1,21 @@
-package com.ssafy.alta.dto.request;/**
+package com.ssafy.alta.dto.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+/**
  * packageName 	: com.ssafy.alta.dto.request
  * fileName 	: UserAlertRequest
  * author 	    : 김유진
  * date		    : 2022-05-17
- * description	: 
- * =========================================================== 
- * DATE 		    AUTHOR 		        NOTE 
- * ----------------------------------------------------------- 
+ * description	:
+ * ===========================================================
+ * DATE 		    AUTHOR 		        NOTE
+ * -----------------------------------------------------------
  * 2022-05-17	        김유진  		        최초 생성
  */
+@Getter
+@ToString
 public class UserAlertRequest {
+    private String alertSetting;
 }
