9a037158f46f27d5c177fe31c9e332dcfbe259ea
WooJeongYeon <wjygogogo@gmail.com>
2022-05-18 15:27:26 +0900

    S06P31B203-504 fix: 🐛 백엔드 - 스레드 풀 설정 부분 삭제

--- backend/alta/src/main/java/com/ssafy/alta/AltaApplication.java
+++ backend/alta/src/main/java/com/ssafy/alta/AltaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class AltaApplication {
--- backend/alta/src/main/java/com/ssafy/alta/util/Scheduling.java
+++ backend/alta/src/main/java/com/ssafy/alta/util/Scheduling.java
@@ -33,20 +33,17 @@ public class Scheduling {
     private final NotificationService notificationService;
     private final Integer ALERT_SAVE_PERIOD = 30;  // 알림 30일 저장
 
-    @Async
     @Scheduled(cron = "0 0 2 * * *") // Cron 표현법 - 매일 2시 실행
     public void scheduleDeleteAlertTask() {
 
         alertService.deleteAlertByPeriod(ALERT_SAVE_PERIOD);
     }
 
-    @Async
     @Scheduled(cron = "0 0 12 * * *") // 매일 12시 실행 - 마감일이 오늘까지인 스터디에 대해 메일 보냄
     public void scheduleSendAlertMailCodeDeadline() throws ParseException, MessagingException {
         scheduleAndProblemService.sendAlertMailCodeDeadline();;
     }
 
-    @Async
     @Scheduled(fixedRate = 60000)  // 60초마다 연결되있다는 용으로 데이터 보냄
     public void sendAlertDummyData() {
         notificationService.sendDummyData();
